0b442f6dc7e6c7a4dead8df75792c978dd67766f
delphine0930 <delphine0930@gmail.com>
2020-09-01 10:34:03 +0900

    Fix mobile host and Add IE9,11

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 ReactDOM.render(
     <App />,
   document.getElementById('root')
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,11 @@
 import Main from '../views/Main.js';
-import Subscribe from '../views/Subscribe.js';
 
 const mainRoutes = [
     {
         name: 'Main',
         path: '/',
         component: Main,
-    },
-    {
-        name: 'Subscribe',
-        path: '/subscribe',
-        component: Subscribe
-    },
+    }
     
 ]
 
